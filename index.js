631d0573c84827a47d68147a4621adb2193b0072
Eric Mathusek <eamathusek@gmail.com>
2019-07-18 00:18:33 -0600

    Testing Testing

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
 // require('dotenv').config();
-// const { Pool } = require("pg")
-const connectionString = process.env.DATABASE_URL ||  process.env.DATABASE_URL1;
-const pool = new Pool({connectionString: connectionString});
+// // const { Pool } = require("pg")
+// const connectionString = process.env.DATABASE_URL ||  process.env.DATABASE_URL1;
+// const pool = new Pool({connectionString: connectionString});
 // const homepageController = require("./model/homepageModel.js");
 
 express()
