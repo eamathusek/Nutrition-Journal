43b8d534ec2023493a6eab3ef028ac8c528c0877
Eric Mathusek <eamathusek@gmail.com>
2019-07-17 23:57:42 -0600

    Testing Testing

--- public/api.js
+++ public/api.js
@@ -54,3 +54,9 @@ function renderExercise(owrkout){
 listWorkout();
 
 
+unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?diet=vegetarian&excludeIngredients=coconut&intolerances=egg%2C+gluten&number=10&offset=0&type=main+course&query=burger")
+.header("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
+.header("X-RapidAPI-Key", "af42b18fbdmsh5cea9dcbd511d68p1add7fjsn750c5b4f114a")
+.end(function (result) {
+  console.log(result.status, result.headers, result.body);
+});
