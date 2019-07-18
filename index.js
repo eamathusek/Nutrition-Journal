cfbb7a3653001e8fdc5026383c0e7b9e9a0d630e
Eric Mathusek <eamathusek@gmail.com>
2019-07-18 00:16:11 -0600

    Testing Testing

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
-require('dotenv').config();
+// require('dotenv').config();
 const { Pool } = require("pg")
 const connectionString = process.env.DATABASE_URL ||  process.env.DATABASE_URL1;
 const pool = new Pool({connectionString: connectionString});
-const homepageController = require("./model/homepageModel.js");
+// const homepageController = require("./model/homepageModel.js");
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -14,7 +14,7 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  .get('/getSwitches', homepageController.getSwitches)
-  .get('/getCaps', homepageController.getCaps)
-  .post('/addKeyboard', homepageController.addKeyboard)
-  .post('/addSwitches', homepageController.addSwitches)
+  // .get('/getSwitches', homepageController.getSwitches)
+  // .get('/getCaps', homepageController.getCaps)
+  // .post('/addKeyboard', homepageController.addKeyboard)
+  // .post('/addSwitches', homepageController.addSwitches)
--- model/homepageModel.js
+++ model/homepageModel.js
@@ -1,24 +1,24 @@
-const { Pool } = require("pg");
-require('dotenv').config();
-const db_url = process.env.DATABASE_URL;
-const pool = new Pool({connectionString: db_url});
-require('util');
+// const { Pool } = require("pg");
+// require('dotenv').config();
+// const db_url = process.env.DATABASE_URL;
+// const pool = new Pool({connectionString: db_url});
+// require('util');
 
-function getNotes(callback){
-    var sql = "SELECT exercise FROM workout"
-    pool.query(sql, function(error, db_info){
-        if(error){
-            console.log("Error in query: ")
-			console.log(error);
-			callback(error, null);
-        } 
-        else {
-            var result = {
-                    success:true,
-                    list:db_info.rows
-            }
-            console.log("Workout Journal result: "+ JSON.stringify(db_info.rows))
-            callback(null, result);
-        }
-    })
-}
+// function getNotes(callback){
+//     var sql = "SELECT exercise FROM workout"
+//     pool.query(sql, function(error, db_info){
+//         if(error){
+//             console.log("Error in query: ")
+// 			console.log(error);
+// 			callback(error, null);
+//         } 
+//         else {
+//             var result = {
+//                     success:true,
+//                     list:db_info.rows
+//             }
+//             console.log("Workout Journal result: "+ JSON.stringify(db_info.rows))
+//             callback(null, result);
+//         }
+//     })
+// }
